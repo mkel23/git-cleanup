1fa35e39ead9f0af7b1d8d37a2ecbc3d591c724b
Mike Kelly <MikeKellyWeb@gmail.com>
2017-10-11 14:46:02 -0500

    Update git_cleanup.sh

--- git_cleanup.sh
+++ git_cleanup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Use this to escape, rather than \e
 esc=$(printf '\033')
+
+arrow=$(echo -ne "${esc}[1;37m||>${esc}[0m ")
 
 delete_branch()
 {
@@ -11,17 +14,45 @@ delete_branch()
 
 prompt_delete()
 {
-    read -rp "${esc}[1;31mDelete branch $line? (Y/n)${esc}[0m " yn </dev/tty
+    read -rp "${arrow}${esc}[1;31mDelete branch $line? (Y/n)${esc}[0m " yn </dev/tty
     case $yn in
         [Yy]* ) delete_branch "$line"; echo; return;;
-        [Nn]* ) echo -e "${esc}[1;37mSkipped $line${esc}[0m\\n"; return;;
+        [Nn]* ) echo -e "${esc}[1;34mSkipped $line${esc}[0m\\n"; return;;
     esac
 }
 
-git branch | while read -r line
-do
-    if [[ $line == *"master"* || $line == *"*"* ]]; then
-        continue
+prune_origin()
+{
+    echo -n "${esc}[1;32m"
+    git remote prune origin
+    echo -n "${esc}[0m"
+}
+
+prompt_prune()
+{
+    if [[ $(git remote prune --dry-run origin) ]]; then
+        echo -n "${esc}[1;33m"
+        git remote prune --dry-run origin
+        echo -e "${esc}[0m"
+        read -rp "${arrow}${esc}[1;31mPrune origin refs? (Y/n)${esc}[0m " yn </dev/tty
+        case $yn in
+            [Yy]* ) echo; prune_origin; echo; return;;
+            [Nn]* ) echo -e "${esc}[1;34mSkipped pruning for origin${esc}[0m\\n"; return;;
+        esac
     fi
-    prompt_delete "$line"
-done
+}
+
+loop_branches()
+{
+    git branch | while read -r line
+    do
+        if [[ $line == *"master"* || $line == *"*"* ]]; then
+            continue
+        fi
+        prompt_delete "$line"
+    done
+}
+
+loop_branches
+prompt_prune
+echo -e "${esc}[1;32mDONE.${esc}[0m"
